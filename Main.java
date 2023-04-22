906b6a16438b3b9585b83b9c4033baf731058885
jahanvir <30360093+jahanvir@users.noreply.github.com>
2023-04-22 19:16:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+		boolean valid=true;
+		scanner sc=new Scanner(System.in);
+
+		do{
+			System.out.println("\nLink state routing\n");
+			System.out.println("Enter the number")
+		}
+    // System.out.println("Hello world!");
+  }
+}
